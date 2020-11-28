Fix entropy computation

compute_entropies.py
@@ -25,10 +25,10 @@ start_time = timeit.default_timer()
 # Iterate over possible lengths of block
 for i in range(1, int(n / 2) + 1):
    rho = np.outer(ground_state, ground_state.getH())
-   rho_ab = rho.reshape(2**i, 2**(n - i), 2**i, 2**(n - i))
+   rho_ab = rho.reshape([2**i, 2**(n - i), 2**i, 2**(n - i)])
    rho_a = np.trace(rho_ab, axis1=1, axis2=3)
    evals = scipy.linalg.eigh(rho_a, eigvals_only=True) 
-   entropy = sum(map(lambda x : 0 if x==0 else -x * math.log(x, 2), evals))
+   entropy = sum(map(lambda x : 0. if np.isclose(x,0.) else -x * math.log(x, 2.), evals))
    entropies.append(entropy)
 tot_time = timeit.default_timer() - start_time
 