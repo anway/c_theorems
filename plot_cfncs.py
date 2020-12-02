09293a2ca04b1560cfdcf60bfcf15d467a24e936
Annie Wei <anniewei@sbcglobal.net>
2020-12-02 00:20:40 -0800

    Fix bug

--- plot_cfncs.py
+++ plot_cfncs.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
-files = ["entropies_8_xxz_1:0.npz", "entropies_10_xxz_1:0.npz", "entropies_12_xxz_1:0.npz", "entropies_14_xxz_1:0.npz", "entropies_16_xxz_1:0.npz", "entropies_18_xxz_1:0.npz"]
+#files = ["entropies_lastex_8_xxz_1:0.npz", "entropies_lastex_10_xxz_1:0.npz", "entropies_lastex_12_xxz_1:0.npz", "entropies_lastex_14_xxz_1:0.npz"]
+files = ["entropies_18_xxz_1:0.npz"]
 for filename in files:
    data = np.load(filename)
-   entropies = [0.] + data["entropies"]
-   delta_entropies = [(entropies[i] - entropies[i-1]) / i for i in range(1, len(entropies))]
+   entropies = np.insert(data["entropies"], 0, 0.)
+   delta_entropies = [np.log2((entropies[i] - entropies[i-1]) * i) for i in range(1, len(entropies))]
    lens = np.arange(1, len(delta_entropies) + 1)
    plt.scatter(lens, delta_entropies)
+   slope, intercept, r_val, p_val, stderr = linregress(lens[2:5], delta_entropies[2:5])
+   print(slope)
 plt.show()
       
--- plot_entropies.py
+++ plot_entropies.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-files = ["entropies_8_xxz_1:0.npz", "entropies_10_xxz_1:0.npz", "entropies_12_xxz_1:0.npz", "entropies_14_xxz_1:0.npz", "entropies_16_xxz_1:0.npz", "entropies_18_xxz_1:0.npz"]
+#files = ["entropies_lastex_8_xxz_1:0.npz", "entropies_lastex_10_xxz_1:0.npz", "entropies_lastex_12_xxz_1:0.npz", "entropies_lastex_14_xxz_1:0.npz"]
+files = ["entropies_18_xxz_1:0.npz"]
 for filename in files:
    data = np.load(filename)
    entropies = data["entropies"]
